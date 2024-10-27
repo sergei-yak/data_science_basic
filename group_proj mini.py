3bd0746f053adee3e4c0fb81978656d225322d79
sergei-yak <79608297+sergei-yak@users.noreply.github.com>
2024-10-27 15:58:50 -0500

    Update group_proj mini.py
    combined both codes

--- group_proj mini.py
+++ group_proj mini.py
@@ -1,9 +1,18 @@
-# Load a dataset
-from sklearn.datasets import load_wine, load_diabetes
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import plotly.graph_objects as go
+from sklearn.linear_model import ElasticNet, LinearRegression
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 #create dataframe pandas from csv file:
 df_binance = pd.read_csv('df_combined.csv') # this data is from binance exchange API btc to usdt price data
@@ -12,14 +21,16 @@ df_binance = pd.read_csv('df_combined.csv') # this data is from binance exchange
 df_binance = df_binance.dropna()
 df_binance = df_binance.reset_index(drop=True)
 df_binance['Event time'] = pd.to_datetime(df_binance['Event time'])
+df_short = df_binance[['Event time', 'Open price', 'High price', 'Low price', 'Close price']]
+df_short = df_short.sort_values(by='Event time')
 
 # EDA and key insights
 print("Summary Statistics:")
-print(df_binance.describe())
-print("Correlation Matrix:")
+print(df_short.describe())
+#print("Correlation Matrix:")
 #print(df_binance.corr())
-print(df_binance.columns)
-print(df_binance.info())
+print(df_short.columns)
+print(df_short.info())
 
 
 #build candlestick chart in plotly
@@ -46,45 +57,224 @@ plt.show()
 
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(df_binance[['mid_price', 'Open price', 'High price', 'Low price']], df_binance[['Close price']], test_size=0.2)
+X_train, X_test, y_train, y_test, event_time_train, event_time_test = train_test_split(
+    df_short[['Open price', 'High price', 'Low price']],
+    df_short['Close price'], 
+    df_short['Event time'],
+    test_size=0.2,
+    random_state=42
+)
 
-# Train a simple linear regression model
+# Scale the features
+scaler = MinMaxScaler(feature_range=(0, 1))
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Define and train the ElasticNet model
+#model = ElasticNet(random_state=42)
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
-# Output the Test Data
-print("Test Data Set:")
-print(X_test)
+# Make predictions on the test set
+predictions = model.predict(X_test_scaled)
 
-# Make predictions
-predictions = model.predict(X_test)
+# Output predictions and actual values for comparison
+predictions_df = pd.DataFrame({
+    'Event time': event_time_test.reset_index(drop=True), 
+    'Predicted': predictions,
+    'Actual': y_test.reset_index(drop=True),
+    'Model': 'ElasticNet'
+})
+predictions_df.reset_index(drop=True, inplace=True)
+print(predictions_df.sort_values(by='Event time'))
 
 # make evaluation metrics
-from sklearn.metrics import mean_squared_error
+#from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 
 # Print evaluation metrics
-print("Mean Squared Error: ", mean_squared_error(y_test, predictions))
-print("Mean Absolute Error: ", mean_absolute_error(y_test, predictions))
-print("R2 Score: ", r2_score(y_test, predictions))
-print("Mean Absolute Percentage Error: ", mean_absolute_error(y_test, predictions)/y_test.mean())
+#print("Mean Squared Error: ", mean_squared_error(predictions_df['Actual'], predictions_df['Predicted']))
+print("Mean Absolute Error: ", mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted']))
+print("R2 Score: ", r2_score(predictions_df['Actual'], predictions_df['Predicted']))
+print("Mean Absolute Percentage Error: ", mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted'])/predictions_df['Actual'].mean())
+
+#create df of evaluation metrics
+df_evaluation = pd.DataFrame({
+    'Mean Absolute Error': [round(mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted']),4)],
+    'R2 Score': [round(r2_score(predictions_df['Actual'], predictions_df['Predicted']),4)],
+    'Mean Absolute Percentage Error': [round((mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted'])/predictions_df['Actual'].mean()),4)],
+    'Model': ['ElasticNet']
+})
 
 
-# Print predictions
-print("Predictions on test data:")
-print(predictions)
-#build a line chart in plotly for predictions
-import plotly.express as px
-fig = px.line(predictions)
-fig = go.Figure()
-predicted_df = pd.DataFrame(predictions, columns=['Predicted Close Price'])
-predicted_df['Actual Close Price'] = y_test.values.flatten()  # Flatten the array to match dimensions
-predicted_df['Event Time'] = df_binance.loc[X_test.index, 'Event time'].values  # Use the corresponding event times
-fig.add_trace(go.Scatter(x=predicted_df['Event Time'], y=predicted_df['Actual Close Price'], mode='lines', name='Actual Close Price', line=dict(color='blue')))
-fig.add_trace(go.Scatter(x=predicted_df['Event Time'], y=predicted_df['Predicted Close Price'], mode='lines', name='Predicted Close Price', line=dict(color='red')))
-fig.update_layout(title='Actual vs Predicted Close Prices',
-                  xaxis_title='Event Time',
-                  yaxis_title='Price (USDT)',
-                  legend_title='Legend')
+# Set seed for reproducibility
+np.random.seed(42)
+torch.manual_seed(42)
+
+# Data Preprocessing
+scaler = MinMaxScaler(feature_range=(0, 1))
+df_scaled = scaler.fit_transform(df_short[['High price', 'Low price', 'Open price',  'Close price']])
+
+X, y = [], []
+time_steps = 60
+for i in range(time_steps, len(df_scaled)):
+    X.append(df_scaled[i - time_steps:i, :])
+    y.append(df_scaled[i, 3]) #for y we use just Close price column
+
+X, y = np.array(X), np.array(y)
+#X = X.reshape((X.shape[0], X.shape[1], 1))
+
+# train-test split
+split_index = int(0.8 * len(X))
+X_train, X_test = X[:split_index], X[split_index:]
+y_train, y_test = y[:split_index], y[split_index:]
+
+# Convert data to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1)
+y_test = torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
+
+# Define LSTM model
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, dropout_rate=0.2):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_rate, bidirectional=True)
+        self.fc1 = nn.Linear(2 * hidden_size, 128)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc2 = nn.Linear(128, output_size)
+
+    def forward(self, x):
+        h_0 = torch.zeros(2 * self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c_0 = torch.zeros(2 * self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        # LSTM forward pass
+        out, _ = self.lstm(x, (h_0, c_0))
+        out = self.fc1(out[:, -1, :])  # Take the last time step's output
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
+
+
+# Hyperparameters
+input_size = 4 #because we have 4 features
+hidden_size = 100
+num_layers = 3
+output_size = 1
+learning_rate = 0.001
+num_epochs = 500
+batch_size = 64
+dropout_rate = 0.2
+
+# Instantiate the model, loss function, and optimizer
+model = LSTMModel(input_size, hidden_size, num_layers, output_size, dropout_rate)
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    model.train()
+    optimizer.zero_grad()
+    output = model(X_train)
+    loss = criterion(output, y_train)
+    loss.backward()
+    optimizer.step()
+    
+    if (epoch + 1) % 5 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.6f}')
+
+# Evaluation and Predictions
+model.eval()
+with torch.no_grad():
+    y_pred = model(X_test)
+
+# Convert predictions and true values back to the original scale
+#y_pred_rescaled = scaler.inverse_transform(y_pred.numpy())
+y_pred_rescaled = scaler.inverse_transform(np.concatenate((np.zeros((y_pred.shape[0], 3)), y_pred.numpy()), axis=1))[:,3]
+#y_test_rescaled = scaler.inverse_transform(y_test.numpy())
+y_test_rescaled = scaler.inverse_transform(np.concatenate((np.zeros((y_test.shape[0], 3)), y_test.numpy()), axis=1))[:,3]
+
+#add new data from LSTM model to predictions_df for comparison
+predictions_df_lstm = pd.DataFrame({
+    'Event time': df_short['Event time'].values[-len(y_pred_rescaled):].ravel(),  # Flatten if needed
+    'Predicted': y_pred_rescaled.ravel(),  # Flatten if needed
+    'Actual': y_test_rescaled.ravel(),  # Flatten if needed
+    'Model': 'LSTM bidirectional'
+})
+
+# Reset index and concatenate
+predictions_df_lstm.reset_index(drop=True, inplace=True)
+#predictions_df = pd.concat([predictions_df, predictions_df_lstm], axis=0)
+predictions_df = pd.merge(predictions_df, predictions_df_lstm, on='Event time', how='inner')
+
+print(predictions_df.columns)
+
+
+# Evaluation metrics
+mse = mean_squared_error(y_test_rescaled, y_pred_rescaled)
+mae = mean_absolute_error(y_test_rescaled, y_pred_rescaled)
+r2 = r2_score(y_test_rescaled, y_pred_rescaled)
+mape = mae / y_test_rescaled.mean()
+
+print("Mean Squared Error: ", mse)
+print("Mean Absolute Error: ", mae)
+print("R2 Score: ", r2)
+print("Mean Absolute Percentage Error: ", mape)
+
+# add new evaluation metrics to the df
+df_evaluation.loc[1] = [round(mae,4), round(r2,4), round(mape,4), 'LSTM bidirectional']
+
+#create dashboard
+from plotly.subplots import make_subplots
+fig = make_subplots(rows=2, cols=2, subplot_titles=("Price of BTC futures", "Close Price Distribution of BTC futures",
+                                    "Evaluation Metrics", "Predicted vs Actual Prices"),
+                                    specs=[[{"type": "xy"}, {"type": "xy"}],[{"type": "domain"}, {"type": "xy"}]
+    ] )
+
+fig.add_trace(go.Candlestick(x=df_short['Event time'],
+                             open=df_short['Open price'],
+                             high=df_short['High price'],
+                             low=df_short['Low price'],
+                             close=df_short['Close price'],
+                             name='Price of BTC futures'),  row=1, col=1)
+fig.add_trace(go.Histogram(x=df_short['Close price'], name='Price Distribution'), row=1, col=2)
+#fig.add_trace(go.Bar(x=['Mean Absolute Error', 'R2 Score', 'Mean Absolute Percentage Error'], y=[mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted']),r2_score(predictions_df['Actual'], predictions_df['Predicted']), mean_absolute_error(predictions_df['Actual'], predictions_df['Predicted'])/predictions_df['Actual'].mean()], name='Evaluation metrics'), row=2, col=1)
+
+fig.add_trace(go.Table(
+        header=dict(
+            values=list(df_evaluation.columns),
+            fill_color='paleturquoise',
+            align='left'
+        ),
+        cells=dict(
+            values=[df_evaluation[col] for col in df_evaluation.columns],
+            fill_color='lavender',
+            align='left'
+        )
+    ),
+    row=2, col=1
+)
+fig.add_trace(go.Scatter(x=predictions_df[predictions_df['Model_y'] == "LSTM bidirectional"].tail(60).sort_values(by='Event time')['Event time'], y=predictions_df.tail(60).sort_values(by='Event time')['Predicted_y'], mode='lines', name='Prediction LSTM'), row=2, col=2)
+fig.add_trace(go.Scatter(x=predictions_df[predictions_df['Model_y'] == "LSTM bidirectional"].tail(60).sort_values(by='Event time')['Event time'], y=predictions_df.tail(60).sort_values(by='Event time')['Actual_y'], mode='lines', name='Actual price'), row=2, col=2)
+fig.add_trace(go.Scatter(x=predictions_df[predictions_df['Model_x'] == "ElasticNet"].tail(60).sort_values(by='Event time')['Event time'], y=predictions_df.tail(60).sort_values(by='Event time')['Predicted_x'], mode='lines', name='Prediction ElesticNet'), row=2, col=2)
+#fig.add_trace(go.Scatter(x=predictions_df[predictions_df['Model_x'] == "ElasticNet"].tail(60).sort_values(by='Event time')['Event time'], y=predictions_df.tail(60).sort_values(by='Event time')['Actual_x'], mode='lines', name='Actual price'), row=2, col=2)
+
+
+
+#add names for subplots x/y axis
+fig.update_yaxes(title_text="Price", row=1, col=1)
+fig.update_yaxes(title_text="Price", row=1, col=2)
+#fig.update_yaxes(title_text="Price", row=2, col=1)
+fig.update_yaxes(title_text="Price", row=2, col=2)
+fig.update_xaxes(title_text="Time", row=1, col=1)
+fig.update_xaxes(title_text="Time", row=1, col=2)
+#fig.update_xaxes(title_text="Time", row=2, col=1)
+fig.update_xaxes(title_text="Time", row=2, col=2)
+fig.update_xaxes(rangeslider_visible=False, row=1, col=1)
+fig.update_layout(height=1000, width=1200, title_text="Time Series Forecasting Dashboard", title_x=0.5)
+
+fig.write_html('df_dashboard.html')
 fig.show()
