0328baee5e28280f753ebb87cbdb9dc0a7853d3a
Shashank-Pabbu <spabbu2277@dbu.edu>
2024-11-03 00:58:36 +0530

    Update group_proj_mini.py

--- group_proj_mini.py
+++ group_proj_mini.py
@@ -1,15 +1,14 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 import plotly.graph_objects as go
 from sklearn.linear_model import ElasticNet, LinearRegression
 import torch
 import torch.nn as nn
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
